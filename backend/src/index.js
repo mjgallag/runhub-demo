6d1b8daecf72d57a20cba98f3a23a441f7d2f653
Michael Gallagher <mjgallag@gmail.com>
2020-12-11 10:14:34 -0800

    feat: use port environment variable

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.NODE_ENV === 'production' ? 8080 : 4000;
+const port = process.env.PORT || 4000;
 
 app.get('/api/message', (req, res) => {
   res.json('Hello World!');
