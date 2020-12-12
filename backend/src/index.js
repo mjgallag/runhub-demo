a0744e7a9953a9e5457a3491014ffa786a8c57a2
Michael Gallagher <mjgallag@gmail.com>
2020-12-11 18:32:00 -0800

    feat(backend): use frontend environment variable

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
+const cors = require('cors');
 const express = require('express');
 
 const app = express();
 const basePath = process.env.RUNHUB_BACKEND_URL ? '' : '/backend';
 const port = process.env.PORT || 4000;
+
+app.use(
+  cors({
+    origin: process.env.RUNHUB_FRONTEND_URL || 'http://localhost:3000',
+  })
+);
 
 app.get(`${basePath}/message`, (req, res) => {
   res.json('Hello World!');
