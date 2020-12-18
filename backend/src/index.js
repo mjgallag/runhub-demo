4ccf39fb2603a6787de1d2c397b9d52c2739c547
Michael Gallagher <mjgallag@gmail.com>
2020-12-18 09:58:50 -0800

    refactor(backend): rename app to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,19 @@
 const cors = require('cors');
 const express = require('express');
 
-const app = express();
+const backend = express();
 const port = process.env.PORT || 4000;
 
-app.use(
+backend.use(
   cors({
     origin: process.env.RUNHUB_SERVICE_FRONTEND_URL || 'http://localhost:3000',
   })
 );
 
-app.get(`/message`, (req, res) => {
+backend.get(`/message`, (req, res) => {
   res.json('Hello World!');
 });
 
-app.listen(port, () => {
+backend.listen(port, () => {
   console.log(`backend listening at http://localhost:${port}`);
 });
